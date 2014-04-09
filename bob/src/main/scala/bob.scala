e69b8e6b33e259f51314ca6a94210d7b9c18a201
Will Mitchell <wvmitchell@gmail.com>
2014-04-09 16:40:02 -0600

    all tests passing, working implementation 1

--- bob/src/main/scala/bob.scala
+++ bob/src/main/scala/bob.scala
@@ -1,15 +1,38 @@
+import scala.util.matching.Regex
+
 class Bob {
 
   def hey(statement: String) : String = {
-    if(shouting(statement)){
-      return "Woah, chill out!"
+    if(silence(statement)){
+      "Fine. Be that way!"
+    } else if(shouting(statement)){
+      "Woah, chill out!"
+    } else if(question(statement)){
+      "Sure."
     } else {
-      return "Whatever."
+      "Whatever."
     }
   }
 
+  def silence(statement: String) : Boolean = {
+    statement.trim == ""
+  }
+
   def shouting(statement: String) : Boolean = {
-    statement == statement.toUpperCase()
+    if(hasLetters(statement)){
+      statement == statement.toUpperCase()
+    } else {
+      false
+    }
+  }
+
+  def question(statement: String) : Boolean = {
+    statement.endsWith("?")
+  }
+
+  def hasLetters(statement: String) : Boolean = {
+    val pattern = new Regex("[a-zA-Z]")
+    pattern.findAllMatchIn(statement).length > 0
   }
 
 }
